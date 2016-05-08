e6657fa05fda37953dce20169d7d20aab1dc2fd7
ernesto ponce <ernesto@pigmalionstudios.com>
2016-05-07 21:27:51 -0300

    Refactor find urls on messages

--- js/app.js
+++ js/app.js
@@ -1,53 +1,85 @@
 var firebase = new Firebase(config.firebaseURL);
-		var firebaseChild = firebase.child("messages");
+var firebaseChild = firebase.child("messages");
 
-		// Custom filters
-		Vue.filter('timeFromNow', function (value) {
-  			return moment(value).fromNow()
-		})
+// Custom filters
+Vue.filter('timeFromNow', function (value) {
+		return moment(value).fromNow()
+});
 
-		new Vue({
-			el: 'body',
+new Vue({
+	el: 'body',
 
-			data:{
-				message: '',
-				messages: []
-			},
+	data:{
+		message: '',
+		messages: [],
+		urlRegex: /(https?:\/\/[^\s]+)/gi
+	},
 
-			created: function(){
-				console.log("view is created");
-				var that = this;
-				// Get messages and show on list
-				firebaseChild.on('child_added', function(childSnapshot, prevChildKey){
-					console.log(childSnapshot.val());
-					that.messages.push(childSnapshot.val());
-				});
-			},
-
-			methods:{
-				addMessage: function(){	
-					var message = this.message;
-					var urlRegex = /(https?:\/\/[^\s]+)/gi;
-					// Get link from text
-					var resp = urlRegex.exec(this.message);
-					if(resp){
-						console.log(resp[0]);
-						// Check if is an image
-						// Get extension
-						var extension = resp[0].split('.').pop();
-						var imagesExt = ["jpg", "jpge", "png", "file"];
-						if(imagesExt.indexOf(extension) != -1){
-							console.log("Is a valid image");
-							message = message.replace(urlRegex, '<br><img src="$1" width="200" height="200" /><br>');
-						}
-					}
-
-					console.log(message);
-					//return;	
-					firebaseChild.push({
-						text: message,
-						date: Firebase.ServerValue.TIMESTAMP
-					});		
-				}
-			}
+	created: function(){
+		console.log("view is created");
+		var that = this;
+		// Get messages and show on list
+		firebaseChild.on('child_added', function(childSnapshot, prevChildKey){
+			console.log(childSnapshot.val());
+			that.messages.push(childSnapshot.val());
 		});
+	},
+
+	methods:{
+		addMessage: function(){	
+			var resp = this.existsLinkOnMessage();
+			if(resp){
+				// Is an image maybe?
+				this.renderImageOnMessage(resp);
+
+			}
+
+			firebaseChild.push({
+				text: this.message,
+				date: Firebase.ServerValue.TIMESTAMP
+			});		
+
+			this.message = '';
+		},
+
+
+		existsLinkOnMessage: function(urlRegex){
+			var resp = this.urlRegex.exec(this.message);
+			return resp;
+		},
+
+		renderImageOnMessage: function(resp){
+			var extension = resp[0].split('.').pop();
+			var imagesExt = ["jpg", "jpge", "png", "file"];
+			if(imagesExt.indexOf(extension) != -1){
+				console.log("is and image");
+				this.message = this.message.replace(this.urlRegex, '<br><img src="$1" width="200" height="200" /><br>');
+			}
+			
+		}
+
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
